5d8bc6a16f60ae2b7f78e50443373a405c04c721
brwitten <bethrwitten@gmail.com>
2017-10-05 15:10:27 -0700

    added first event listener

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ console.log("I'm linked!");
 $(document).ready(function(){
   console.log("jquery ready!");
 
+  $('#box1').on('click', imageAppear);
+
+  function imageAppear(event) {
+    alert("hello!");
+    console.log("function called");
+  };
+
       function Card(options) {
         this.face = options.face;
         this.isFlipped = options.isFlipped;
@@ -20,6 +27,19 @@ $(document).ready(function(){
         // match function
       };
 
-var newCard = new Card({face: "JUstin", isFlipped: false, matchFound: false});
-console.log(newCard)
+var Justin1 = new Card({face: "Justin", isFlipped: false, matchFound: false});
+var Justin2 = new Card({face: "Justin", isFlipped: false, matchFound: false});
+var Kay1 = new Card({face: "Kay", isFlipped: false, matchFound: false});
+var Kay2 = new Card({face: "Kay", isFlipped: false, matchFound: false});
+var Masha1 = new Card({face: "Masha", isFlipped: false, matchFound: false});
+var Masha2 = new Card({face: "Masha", isFlipped: false, matchFound: false});
+var Beth1 = new Card({face: "Beth", isFlipped: false, matchFound: false});
+var Beth2 = new Card({face: "Beth", isFlipped: false, matchFound: false});
+var Nick1 = new Card({face: "Nick", isFlipped: false, matchFound: false});
+var Nick2 = new Card({face: "Nick", isFlipped: false, matchFound: false});
+var Kenneth1 = new Card({face: "Kenneth", isFlipped: false, matchFound: false});
+var Kenneth2 = new Card({face: "Kenneth", isFlipped: false, matchFound: false});
+
+console.log(Nick1);
+
 });

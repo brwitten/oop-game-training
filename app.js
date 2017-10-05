7834916e0885db3fba5354b1bb164293bbe24ae2
brwitten <bethrwitten@gmail.com>
2017-10-05 14:14:30 -0700

    added board to HTML and CSS files

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("I'm linked!")
+console.log("I'm linked!");
+
+function Card(option) {
+  this.img = option.img;
+  this.isFlipped = option.isFlipped;
+  this.matchFound = option.matchFound;
+};
+
+function User(option) {
+  this.score = option.score;
+  this.name = option.name;
+};
+
+function Game(option) {
+  this.spot = option.spot;
+  // shuffle function
+  // match function
+};
